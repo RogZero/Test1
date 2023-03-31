bdf7782709c27844db5789ef317d7d3ad0642c55
RogZero <program0zero@gmail.com>
2023-03-31 17:47:25 +0300

    Second Commit -TEST

--- Program.cs
+++ Program.cs
@@ -14,6 +14,36 @@ class Program
         
         // Get a value from Redis
         string? value = db.StringGet("mykey");
-        TESTTTTTTTTTTT
+        Console.WriteLine(value);
+
+
+        var connectionString = "Server=BigLen;Port=5432;Database=locus;User Id=postgres;Password=8352;";
+
+        using var connection = new NpgsqlConnection(connectionString);
+
+        connection.Open();
+
+        // Create a command to retrieve the data from a table
+        {
+            // Create a command to retrieve the lists of tables
+            // var command = new NpgsqlCommand("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name;", connection);
+        }
+        var command = new NpgsqlCommand("SELECT * FROM users;", connection);
+
+        // Execute the command
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                Console.Write(reader.GetValue(i) + " ");
+        }
+            Console.WriteLine();
+        }
+
+        // Close the connection
+        connection.Close();
+
+        // TEST2
     }
 }
